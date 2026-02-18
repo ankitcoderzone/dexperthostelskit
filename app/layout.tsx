779ce0b7d5622926ebd4b771566032b3e95e8e35
ankitcoderzone <ankityadav559008@gmail.com>
2026-02-19 03:45:19 +0530

    speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -71,6 +72,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Analytics />
+        <SpeedInsights/>
         <Footer />
         <Toaster position="top-center" richColors />
       </body>
