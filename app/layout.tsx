045e6cf52fea54c5c2e44d5be564063d7c8cfefc
ankitcoderzone <ankityadav559008@gmail.com>
2026-05-25 05:38:07 +0530

    ga

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,6 +66,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2336020427462453"
+     crossOrigin="anonymous"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
